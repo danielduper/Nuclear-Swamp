4418839ff3dcd5de24fae622184b90796d013490
DDANIEL! <dmarttos8@gmail.com>
2025-07-08 15:37:29 -0300

    0.1.1

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const fotoNav = document.querySelector("img");
+
+fotoNav.addEventListener("click", () => {
+
+    const gear = fotoNav.getAttribute("src");
+
+    if(gear === "/assets/images/site-images/firefox.png/"){
+        fotoNav.setAttribute("src", "/assets/images/site-images/chrome.png/");
+    }else{
+        fotoNav.setAttribute("src","/assets/images/site-images/firefox.png/");
+    }
+});
+
+//------------------------------------
+
+let botao = document.querySelector("button");
+let titulo = document.querySelector("h1");
+
+function setarNome() {
+    const meuNome = prompt("Insira seu nome de usuário plsss");
+    if (!meuNome) {
+        setarNome();
+    }else{
+    localStorage.setItem("nome", meuNome);
+    titulo.textContent = `welcome, ${meuNome}`;
+}}
+
+botao.addEventListener("click", () => {
+    setarNome();
+});
+
+if (!localStorage.getItem("nome")){
+    setarNome();
+}else{
+    const nomeGuardado = localStorage.getItem("nome");
+    titulo.textContent = `welcome, ${nomeGuardado}`;
+}
